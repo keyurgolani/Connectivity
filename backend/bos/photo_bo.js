cf74690eff21528b0d1e6813bfdcef080ada930e
Keyur Golani <keyurrgolani@gmail.com>
2017-05-20 14:17:12 -0700

    Timeline shows photo and screenname now. Also added method to fetch photo from photoid

---
+++ backend/bos/photo_bo.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+var dao = require('../utils/dao');
+var bcrypt = require("bcrypt");
+var logger = require("../utils/logger");
+var async = require("async");
+var moment = require('moment');
+
+
+module.exports.getPhoto = function(db, photoID, processResult) {
+	db.get('photos').find({
+		'_id': new ObjectID(photoID)
+	}).then(processResult, function(error) {
+		throw error;
+	});
+};
