fdcd79aaf5b11270ed48d1b02887740de651110d
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-05-20 21:19:00 -0700

    temp user profile navigation

---
+++ Connectivity/app/src/main/java/edu/sjsu/cmpe/fourhorsemen/connectivity/activities/OtherUserProfileActivity.java
@@ -0,0 +1,29 @@
+package edu.sjsu.cmpe.fourhorsemen.connectivity.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import edu.sjsu.cmpe.fourhorsemen.connectivity.R;
+
+public class OtherUserProfileActivity extends AppCompatActivity {
+
+    private static String TAG = OtherUserProfileActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
