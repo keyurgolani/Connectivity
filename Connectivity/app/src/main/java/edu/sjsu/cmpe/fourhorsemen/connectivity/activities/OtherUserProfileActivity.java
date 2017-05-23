4f8e4b2bad080a845ffca564a6f7b2549fefe859
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-05-23 15:33:46 -0700

    last commit

--- Connectivity/app/src/main/java/edu/sjsu/cmpe/fourhorsemen/connectivity/activities/OtherUserProfileActivity.java
+++ Connectivity/app/src/main/java/edu/sjsu/cmpe/fourhorsemen/connectivity/activities/OtherUserProfileActivity.java
@@ -1,15 +1,92 @@
 package edu.sjsu.cmpe.fourhorsemen.connectivity.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.sjsu.cmpe.fourhorsemen.connectivity.R;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.fragments.AboutFragment;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.fragments.PostFragment;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.fragments.ProfileFragment;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.utilities.PreferenceHandler;
 
 public class OtherUserProfileActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_profile);
+
+        viewPager = (ViewPager) findViewById(R.id.tab_viewpager);
+        if (viewPager != null){
+            ViewPagerAdapter adapter = new ViewPagerAdapter(this.getFragmentManager());
+            adapter.addFrag(AboutFragment.newInstance(), "About");
+            adapter.addFrag(PostFragment.newInstanceForProfile(1, Integer.parseInt(PreferenceHandler.getProfileID())), "My Posts");
+            adapter.addFrag(new PostFragment(), "Albums");
+            viewPager.setAdapter(adapter);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+
+        public ViewPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+
 }
--- Connectivity/app/src/main/java/edu/sjsu/cmpe/fourhorsemen/connectivity/activities/SearchFriendsActivity.java
+++ Connectivity/app/src/main/java/edu/sjsu/cmpe/fourhorsemen/connectivity/activities/SearchFriendsActivity.java
@@ -1,43 +1,42 @@
 package edu.sjsu.cmpe.fourhorsemen.connectivity.activities;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import edu.sjsu.cmpe.fourhorsemen.connectivity.R;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.beans.Post;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.beans.Profile;
+import edu.sjsu.cmpe.fourhorsemen.connectivity.fragments.MyPostRecyclerViewAdapter;
 import edu.sjsu.cmpe.fourhorsemen.connectivity.utilities.PreferenceHandler;
 import edu.sjsu.cmpe.fourhorsemen.connectivity.utilities.ProjectProperties;
 import edu.sjsu.cmpe.fourhorsemen.connectivity.utilities.RequestHandler;
 import edu.sjsu.cmpe.fourhorsemen.connectivity.utilities.ResponseHandler;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.WindowManager;
+
 
 public class SearchFriendsActivity extends AppCompatActivity {
 
     private static String TAG = SearchFriendsActivity.class.getSimpleName();
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter mAdapter;
 
     @Bind(R.id.search_email) EditText searchEmail;
     @Override
@@ -60,14 +59,60 @@ public class SearchFriendsActivity extends AppCompatActivity {
 
                         //TODO: Search Code here
 
+
                         return true;
                     }
                 }
                 return false;
             }
         });
+
+        // Set the adapter
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //mAdapter = new MyPostRecyclerViewAdapter(getPersonalTimeline(getContext()), mListener);
+        recyclerView.setAdapter(mAdapter);
+
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private List<Post> getSearchedFriends(final Context context) {
+        final List<Post> personalTimeline = new ArrayList<Post>();
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("unique_id", PreferenceHandler.getAccessKey());
+
+        RequestHandler.HTTPRequest(context, ProjectProperties.METHOD_FETCH_TIMELINE, params, new ResponseHandler() {
+            @Override
+            public void handleSuccess(JSONObject response) throws Exception {
+                if(response.getInt("status_code") == 200) {
+                    JSONArray posts = response.getJSONArray("message");
+                    for(int i  = 0; i < posts.length(); i++) {
+                        JSONObject currentObj = posts.getJSONObject(i);
+                        personalTimeline.add(new Post(currentObj.getInt("post_id"), currentObj.getString("photo"), currentObj.getString("post"), new Profile(currentObj.getInt("profile"), currentObj.getString("profile_pic"), currentObj.getString("screen_name")), currentObj.getString("timestamp")));
+                    }
+                    mAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(context, "Internal Error. Please try again later.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void handleError(Exception e) throws Exception {
+                e.printStackTrace();
+            }
+        });
+        return personalTimeline;
+    }
 
 }
